9b0cbb9db7fbe7a2680a83ba203d45c0f8960701
frendikawa <frsnaifla@gmail.com>
2024-01-23 13:00:13 +0700

    hps tbl hlmn trli

--- breezy/resources/views/troli.blade.php
+++ breezy/resources/views/troli.blade.php
@@ -48,25 +48,5 @@
             </div>
         </div>
         @endforeach
-        <div class="table-responsive">
-            <table class="table table-hover">
-                <thead>
-                    <tr>
-                        <th>No.</th>
-                        <th>Gambar Produk</th>
-                        <th>Nama Produk</th>
-                        <th>Harga Produk</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>ini gambar</td>
-                        <td>Eren yeager</td>
-                        <td>75.000</td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
     </div>
 @endsection
